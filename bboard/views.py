b065fb914e265e2417c828a3dd4dd44f0347a806
IgorOsiyuk <rigidum.by@gmail.com>
2025-12-04 19:44:32 +0300

    urls and templates

--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,7 @@
 # from django.http import HttpResponse
 # from django.template import loader
 from django.shortcuts import render
-from .models import Bb
+from .models import Bb, Rubric
 
 '''
 использую без shortcuts
@@ -18,5 +18,15 @@ from .models import Bb
 '''
 def index(request):
     bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs':bbs, 'rubrics':rubrics}
 
-    return render(request,'bboard/index.html', {'bbs': bbs})
+    return render(request,'bboard/index.html', context)
+
+def rubric_bbs(request, rubric_id):
+    bbs=Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs':bbs, 'rubrics':rubrics, 'current_rubric':current_rubric}
+
+    return render(request,'bboard/rubric_bbs.html', context)
