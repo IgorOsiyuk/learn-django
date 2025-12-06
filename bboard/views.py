d24479212b8673f9c8763127b29d37ec006976ba
IgorOsiyuk <rigidum.by@gmail.com>
2025-12-06 17:08:04 +0300

    add form

--- bboard/views.py
+++ bboard/views.py
@@ -2,7 +2,9 @@
 # from django.template import loader
 from django.shortcuts import render
 from .models import Bb, Rubric
-
+from django.views.generic import CreateView
+from .forms import BbForm
+from django.urls import reverse_lazy
 '''
 использую без shortcuts
 '''
@@ -30,3 +32,13 @@ def rubric_bbs(request, rubric_id):
     context = {'bbs':bbs, 'rubrics':rubrics, 'current_rubric':current_rubric}
 
     return render(request,'bboard/rubric_bbs.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/bb_create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
